f39d7f9fa1aa85b856ab5579822746751cef2da9
黎摄文 <lishewen@gmail.com>
2020-02-03 09:54:54 +0800

    添加项目文件。

---
+++ CommonVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
